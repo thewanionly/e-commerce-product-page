3b13306ea16eda15969f442cc9a575efc1fca56e
thewanionly <wani.pitogo@gmail.com>
2021-11-19 06:31:46 +0800

    Working Icon component (import svg icons as react component through SVGR then provide mapping of "name" prop)

--- src/components/atoms/Icon/Icon.jsx
+++ src/components/atoms/Icon/Icon.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import CartSVG from '../../../assets/images/icon-cart.svg'
+import IconCartSvg from 'assets/images/icon-cart.svg'
+import IconCloseSvg from 'assets/images/icon-close.svg'
+import IconDeleteSvg from 'assets/images/icon-delete.svg'
+import IconMenuSvg from 'assets/images/icon-menu.svg'
+import IconMinusSvg from 'assets/images/icon-minus.svg'
+import IconNextSvg from 'assets/images/icon-next.svg'
+import IconPlusSvg from 'assets/images/icon-plus.svg'
+import IconPreviousSvg from 'assets/images/icon-previous.svg'
+import LogoSvg from 'assets/images/logo.svg'
 
-const Icon = ({ src, name, type, fill }) => {
-  return type === 'svg' ? <CartSVG className='cartSvg' /> : <img src={src} alt={name} />
+/** Maps name to its corresponding icon */
+const ICONS = {
+  cart: IconCartSvg,
+  close: IconCloseSvg,
+  delete: IconDeleteSvg,
+  menu: IconMenuSvg,
+  minus: IconMinusSvg,
+  next: IconNextSvg,
+  plus: IconPlusSvg,
+  previous: IconPreviousSvg,
+  logo: LogoSvg
+}
+
+const Icon = ({ className = '', name }) => {
+  const IconComponent = ICONS[name]
+
+  return <>{IconComponent && <IconComponent className={className} />}</>
 }
 
 Icon.propTypes = {
-  src: PropTypes.string,
+  className: PropTypes.string,
   name: PropTypes.string
 }
 
