235e4e9465de062719f2ceea3c5a14087c954ceb
thewanionly <wani.pitogo@gmail.com>
2021-11-22 02:40:16 +0800

    Use Text in Header

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -2,20 +2,55 @@ import React from 'react'
 
 import Icon from 'components/atoms/Icon'
 import Image from 'components/atoms/Image'
+import Text from 'components/atoms/Text'
 
 import avatarImg from 'assets/images/image-avatar.png'
 
 const Header = () => {
+  const navLinks = [
+    {
+      text: 'Collections',
+      name: 'collections'
+    },
+    {
+      text: 'Men',
+      name: 'men'
+    },
+    {
+      text: 'Women',
+      name: 'women'
+    },
+    {
+      text: 'About',
+      name: 'about'
+    },
+    {
+      text: 'Contact',
+      name: 'contact'
+    }
+  ]
+
+  const handleNavLinkClick = name => {
+    console.log('handleNavLinkClick', name)
+  }
+
   return (
     <header className='header'>
       <div className='header-nav'>
         <Icon name='logo' />
         <nav className='header-nav-links'>
-          <span className='header-nav-link'>Collections</span>
-          <span className='header-nav-link'>Men</span>
-          <span className='header-nav-link'>Women</span>
-          <span className='header-nav-link'>About</span>
-          <span className='header-nav-link'>Contact</span>
+          {navLinks.map(({ text, name }) => (
+            <Text
+              key={name}
+              className='header-nav-link'
+              size='small'
+              lineHeight='large-1-lh'
+              color='secondary'
+              onClick={() => handleNavLinkClick(name)}
+            >
+              {text}
+            </Text>
+          ))}
         </nav>
         <Icon name='cart' />
         <Image src={avatarImg} alt='avatar' size='small' isAvatar />
