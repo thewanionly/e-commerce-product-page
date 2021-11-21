03d6fba4f5b75bd2a4f4912103147db76e2954a6
thewanionly <wani.pitogo@gmail.com>
2021-11-22 02:40:04 +0800

    Created Text component (atom)

---
+++ src/components/atoms/Text/Text.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+/** Text sizes map */
+// 12px (smallest)
+// 13px (smaller)
+// 15px (small)
+// 16px (base)
+// 18px (large)
+// 28px (larger)
+// 44px (largest)
+
+/** Text weight map */
+// normal ($body-font-weight-normal)
+// bold ($body-font-weight-bold)
+
+/** Text color map */
+// primary ($grayish-blue-darkest)
+// secondary ($grayish-blue-dark)
+
+// add overflow styles
+
+/** Line heights */
+// 12px (smallest)
+// 13px (smaller)
+// 15px (small)
+// 16px (base)
+// 25px (medium)
+// 26px (large-1)
+// 28px (large-2)
+// 32px (larger)
+// 48px (largest)
+
+const Text = ({
+  className = '',
+  size = 'base',
+  weight = 'normal',
+  lineHeight = 'base-lh',
+  color = 'primary',
+  isAllCaps,
+  isTitleCase,
+  onClick,
+  children
+}) => {
+  const isClickable = !!onClick
+  const textClassName = `text ${className} ${size} ${weight} ${lineHeight} ${color} ${
+    isClickable ? 'clickable' : ''
+  }`
+
+  return (
+    <span className={textClassName} onClick={onClick}>
+      {children}
+    </span>
+  )
+}
+
+Text.propTypes = {
+  className: PropTypes.string,
+  size: PropTypes.string,
+  weight: PropTypes.string,
+  color: PropTypes.string,
+  isAllCaps: PropTypes.bool,
+  isTitleCase: PropTypes.bool,
+  children: PropTypes.node
+}
+
+export default Text
