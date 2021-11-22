c3f9296b2e1771e1e67c48e09de620cb334b7d52
thewanionly <wani.pitogo@gmail.com>
2021-11-23 06:31:46 +0800

    Overlay component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 
 import Header from 'components/organisms/Header'
 import MenuSideBar from 'components/organisms/MenuSideBar'
+import Overlay from 'components/organisms/Overlay'
 
 const App = () => {
   const [showMenu, setShowMenu] = useState(false)
 
   return (
     <div className='app'>
+      {showMenu && <Overlay />}
       <MenuSideBar showMenu={showMenu} setShowMenu={setShowMenu} />
       <Header setShowMenu={setShowMenu} />
       <main className='main'></main>
