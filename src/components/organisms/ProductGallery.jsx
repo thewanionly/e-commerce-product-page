f48e1439f6218bcc113f06dfc00bacc98d84b536
thewanionly <wani.pitogo@gmail.com>
2021-11-30 05:31:26 +0800

    Show next and previous buttons in mobile view

--- src/components/organisms/ProductGallery.jsx
+++ src/components/organisms/ProductGallery.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
+import Icon from 'components/atoms/Icon'
 import Image from 'components/atoms/Image'
 
 import product1Img from 'assets/images/image-product-1.jpg'
@@ -14,24 +15,31 @@ import product4ImgThumbnail from 'assets/images/image-product-4-thumbnail.jpg'
 const images = [
   {
     thumbnail: product1ImgThumbnail,
-    main: product1Img
+    main: product1Img,
+    imageIndex: 1
   },
   {
     thumbnail: product2ImgThumbnail,
-    main: product2Img
+    main: product2Img,
+    imageIndex: 2
   },
   {
     thumbnail: product3ImgThumbnail,
-    main: product3Img
+    main: product3Img,
+    imageIndex: 3
   },
   {
     thumbnail: product4ImgThumbnail,
-    main: product4Img
+    main: product4Img,
+    imageIndex: 4
   }
 ]
 
 const ProductGallery = ({ setShowLightBox }) => {
   const [mainImage, setMainImage] = useState(product1Img)
+  const [currImgIndex, setCurrImgIndex] = useState(
+    images.find(({ main }) => main === mainImage).imageIndex
+  )
 
   const handleOpenLightBox = () => {
     setShowLightBox(true)
@@ -41,6 +49,42 @@ const ProductGallery = ({ setShowLightBox }) => {
     setMainImage(img)
   }
 
+  const handleGoToNextImage = e => {
+    e.stopPropagation()
+
+    let currIndex = currImgIndex
+
+    if (currIndex === images.length) {
+      currIndex = 1
+    } else {
+      currIndex++
+    }
+
+    setCurrImgIndex(currIndex)
+  }
+
+  const handleGoToPreviousImage = e => {
+    e.stopPropagation()
+
+    let currIndex = currImgIndex
+
+    if (currIndex === 1) {
+      currIndex = 4
+    } else {
+      currIndex--
+    }
+
+    setCurrImgIndex(currIndex)
+  }
+
+  useEffect(() => {
+    setMainImage(images.find(({ imageIndex }) => imageIndex === currImgIndex).main)
+  }, [currImgIndex])
+
+  useEffect(() => {
+    setCurrImgIndex(images.find(({ main }) => main === mainImage).imageIndex)
+  }, [mainImage])
+
   return (
     <div className='product-gallery'>
       <Image
@@ -49,6 +93,12 @@ const ProductGallery = ({ setShowLightBox }) => {
         alt='main-image'
         onClick={() => handleOpenLightBox()}
       />
+      <div className='product-gallery__previous-icon__container' onClick={handleGoToPreviousImage}>
+        <Icon className='product-gallery__previous-icon' name='previous' />
+      </div>
+      <div className='product-gallery__next-icon__container' onClick={handleGoToNextImage}>
+        <Icon className='product-gallery__next-icon' name='next' />
+      </div>
       <div className='product-gallery__secondary-images'>
         {images.map(({ thumbnail, main }, index) => (
           <Image
